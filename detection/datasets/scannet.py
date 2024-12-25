dba6dccb3f51bd97770e7b2715f821a4e2cf7d04
zyh16143998882 <1614399882@qq.com>
2024-12-25 11:59:06 +0800

    Update Detection

--- detection/datasets/scannet.py
+++ detection/datasets/scannet.py
@@ -20,8 +20,8 @@ from utils.random_cuboid import RandomCuboid
 
 IGNORE_LABEL = -100
 MEAN_COLOR_RGB = np.array([109.8, 97.2, 83.8])
-DATASET_ROOT_DIR = "/data1/zhayaohua/data/scannet/scannet_train_detection_data"  ## Replace with path to dataset
-DATASET_METADATA_DIR = "/data1/zhayaohua/data/scannet/meta_data" ## Replace with path to dataset
+DATASET_ROOT_DIR = ""  ## Replace with path to dataset
+DATASET_METADATA_DIR = "" ## Replace with path to dataset
 
 
 class ScannetDatasetConfig(object):
